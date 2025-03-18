46f64c8c91508757a7f202dd4406f180b5ea6ac0
TeamFour-Coder <collegeprojectt26@gmail.com>
2025-03-18 12:06:28 +0530

    updated frontend mistake

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -254,7 +254,7 @@ const CoursesPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
+      {/* <Navbar /> */}
       
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="bg-indigo-700 rounded-xl p-8 mb-8">
